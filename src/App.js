17e2887457143a8812769f3e1176069d81449579
Bhupendra <Bhupentiwari@live.com>
2021-05-02 01:22:19 +0530

    ADD : header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { Counter } from './features/counter/Counter';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     Hello
+    <Header />
     </div>
   );
 }
